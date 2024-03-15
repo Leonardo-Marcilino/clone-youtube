0d26fb75e9521801ae7729c35f8210ef9841fb84
Leo emhotep <126977738+Leonardo-Marcilino@users.noreply.github.com>
2024-03-14 23:00:48 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+function mostra_scroll(){
+    document.getElementById("bx-menu").style.overflow="auto"; 
+}
+
+
+function some_scroll(){
+    document.getElementById("bx-menu").style.overflow="hidden";
+}
